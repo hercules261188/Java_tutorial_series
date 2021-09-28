2c574c441c8fcf168896d1a86e486af813a0b074
Knowledge Shelf <60973040+knowledgeshelfit@users.noreply.github.com>
2021-09-28 23:38:30 +0530

    Add files via upload

---
+++ Tutorial26/Main.java
@@ -0,0 +1,14 @@
+package com.vishwajeet;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Bike hero = new Bike();
+
+        hero.setModel("007");
+        System.out.println("Bike model is " + hero.getModel());
+
+
+
+    }
+}
